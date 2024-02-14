efaa7223498bfa38ff47f89b56d364e8c60f6e8b
Guilherme <guilhermesilba811@gmail.com>
2024-02-14 09:09:15 -0300

    css do resultado e btn de reiniciar quiz

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,37 @@ const reducer = (state, action) => {
     return {...state, apiData: action.apiData}
   } 
   if(action.type === 'clicked_option') {
-    return {...state, clickedOption: action.index}
+    return {
+      ...state,
+      clickedOption: action.index,
+      userScore: action.index === state.apiData[state.currentQuestion].correctOption
+         ? state.userScore + state.apiData[state.currentQuestion].points
+         : state.userScore
+    }
   }
+   if(action.type === 'clicked_next_question') {
+     const isLastQuestion = state.currentQuestion + 1 === state.apiData.length;
+     return {
+      ...state,
+      currentQuestion: isLastQuestion ? 0 : state.currentQuestion + 1,
+      clickedOption: null,
+      shouldShowResult: isLastQuestion
+     }
+   }  
+
+   if(action.type === 'clicked_restart') {
+    return {...state, userScore:0, shouldShowResult: false};
+   }
 
   return state;
 }
 
+const initialState =  {currentQuestion: 0, apiData: [], clickedOption: null, userScore:0,
+shouldShowResult: false};
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, {currentQuestion: 0, apiData: [], clickedOption: null});
+  const [state, dispatch] = useReducer(reducer, initialState);
+ 
 
   useEffect(() => {
     fetch('https://raw.githubusercontent.com/Roger-Melo/fake-data/main/videogame-questions.json')
@@ -23,11 +45,28 @@ const App = () => {
   }, []);
 
   const handleClickOption = index => dispatch({type: 'clicked_option', index});
+  const handleClickNextQuestion = () => dispatch({type: 'clicked_next_question'});
+  const handleClickRestart = () => dispatch({type: 'clicked_restart'});
+  const userHasAnswered = state.clickedOption !== null;
+  const maxScore = state.apiData.reduce((acc, q) => acc + q.points, 0);
+  const percentage = state.userScore / maxScore * 100;
 
   return (
     <div className='app'>
+      <header className='app-header'>
+        <img src="./img/logo-quiz-videogames.png" alt="logo do quiz" />
+        <h1>Quiz dos Videogames</h1>
+      </header>
       <main>
-        {state.apiData.length > 0 && (
+        {state.shouldShowResult && (
+          <>
+             <div className='result'>
+               <span>Você fez <b>{state.userScore}</b> pontos de {maxScore} ({percentage}%)</span>
+             </div>
+             <button onClick={handleClickRestart} className='btn btn-ui'>Reinicar quiz</button>
+          </>
+        )}
+        {state.apiData.length > 0 && !state.shouldShowResult && (
           <>
             <h4>{state.apiData[state.currentQuestion].question}</h4>
             <ul className='options'>
@@ -38,7 +77,7 @@ const App = () => {
                        btn 
                        btn-option 
                        ${state.clickedOption === index ? 'answer' : ''}
-                       ${state.clickedOption !== null
+                       ${userHasAnswered
                          ? (state.apiData[state.currentQuestion]?.correctOption === index
                           ? 'correct'
                           : 'wrong'
@@ -46,13 +85,17 @@ const App = () => {
                           :''
                          } 
                        `}
-                   disabled={state.clickedOption !== null}
+                   disabled={userHasAnswered}
                    >{option}
                    </button>
                    </li>)}
             </ul>
            <div>
-            <button className='btn btn-option'>Próxima</button>
+            {userHasAnswered && (
+               <button onClick={handleClickNextQuestion} className='btn btn-option btn-ui'>
+                {state.currentQuestion === state.apiData.length - 1 ? 'Finalizar' : 'Próxima'}
+               </button>
+            )}
            </div>
           </>
         )}
